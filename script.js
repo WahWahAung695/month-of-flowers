fe025a5fd0b853110dd1a7e6ebc774d464d8e83c
wahwahaung <wahwahaung695@gmail.com>
2024-10-30 23:15:29 +0630

    add back to top button

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ document.querySelector('form').addEventListener('submit', function(event) {
     var modal = new bootstrap.Modal(document.getElementById('thankYouModal'));
     modal.show();
 });
+
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
